d74ffeb7619af9e4393fca070527c6285538f3d5
Steven Lu (MBP M1 Max[tmux]) <1542910+unphased@users.noreply.github.com>
2024-06-26 02:51:40 -0400

    lets see if this will let me commit now

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    // You can add custom rules here
+  },
+};
