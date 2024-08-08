6538b8831ae89e1461314ee8a421faf295e49d9f
Steven Lu @ MBP-M1-Max <1542910+unphased@users.noreply.github.com>
2024-08-08 13:13:07 -0400

    - old bugs fixed - new bugs added

--- color/rainbow_hsluv.ts
+++ color/rainbow_hsluv.ts
@@ -5,6 +5,6 @@ export const rainbow_hsluv = (num: number, hue_offset = -60, hue_range_ratio = 0
   const HUE_RANGE = hue_range_ratio * 360;
   const HUE_DEG_PER_STOP = HUE_RANGE / (NUM_COLORS - 1);
   const hues = Array.from({ length: NUM_COLORS }, (_, i) => ((i * HUE_DEG_PER_STOP + hue_offset) + 720) % 360);
-  return hues.map(h => hsluv2rgb(h, 100, 70));
+  return hues.map(h => hsluv2rgb(h, 100, 70)).map(([r, g, b]) => [Math.max(0, r), Math.max(0, g), Math.max(0, b)]);
 };
 
