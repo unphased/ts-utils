b4190bacce2dc94cfd0a996afdb0ae8b82b87738
Steven Lu @ MBP-M1-Max <1542910+unphased@users.noreply.github.com>
2024-09-04 14:03:20 -0400

    prep for publish

--- src/watch/index.ts
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from 'url';
-
-const isProgramLaunchContext = () => {
-  return fileURLToPath(import.meta.url) === process.argv[1];
-}
-
-if (isProgramLaunchContext()) {
-
-}
