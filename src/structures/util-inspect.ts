dde531a028aaacdde3fc30333f0f409865e9e9e4
Steven Lu @ MBP-M1-Max <1542910+unphased@users.noreply.github.com>
2024-10-15 22:55:10 -0400

    fix: simplify util-inspect.ts to avoid newline issues

--- src/structures/util-inspect.ts
+++ src/structures/util-inspect.ts
@@ -403,22 +403,11 @@ function formatPrimitive(ctx, value) {
 }
 
 function reduceToSingleString(output, base, braces) {
-  var numLinesEst = 0;
   var length = reduce(output, function(prev, cur) {
-    numLinesEst++;
-    if (cur.indexOf('\n') >= 0) numLinesEst++;
     return prev + cur.replace(/\u001b\[\d\d?m/g, '').length + 1;
   }, 0);
 
-  if (length > 60) {
-    return braces[0] +
-           (base === '' ? '' : base + '\n ') +
-           ' ' +
-           output.join(',\n  ') +
-           ' ' +
-           braces[1];
-  }
-
+  // Always return a single-line string
   return braces[0] + base + ' ' + output.join(', ') + ' ' + braces[1];
 }
 
